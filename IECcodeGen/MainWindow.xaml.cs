e5c267eeddbf344a5f762c75d074f5d5ffbc54a5
Remo Scheidegger <resc@mailbox.org>
2019-05-12 06:58:44 +0200

    Mit Einstellungen

--- IECcodeGen/MainWindow.xaml.cs
+++ IECcodeGen/MainWindow.xaml.cs
@@ -47,29 +47,44 @@ namespace IECcodeGen
             xshd_stream.Close();
 
             text_code_template.ShowLineNumbers = true;
-            text_code_template.TextArea.Foreground = Brushes.White;
-            text_code_template.TextArea.FontSize = 13;
             text_code_template.TextArea.FontFamily = new FontFamily("Consolas");
             text_code_template.Options.ConvertTabsToSpaces = true;
 
             text_code_output.IsReadOnly = true;
             text_code_output.TextArea.Caret.CaretBrush = Brushes.Transparent;
-            text_code_output.TextArea.Foreground = Brushes.White;
-            text_code_output.TextArea.FontSize = 13;
             text_code_output.TextArea.FontFamily = new FontFamily("Consolas");
-            text_code_output.ShowLineNumbers = true;
             text_code_output.Options.ConvertTabsToSpaces = true;
 
-            //Theme
-            ThemeManager.ChangeAppStyle(Application.Current,
-                ThemeManager.GetAccent("Steel"),
-                ThemeManager.GetAppTheme("BaseDark"));
 
             //ComboBox
-
             combo_vars.Items.Add("Variable_1");
             combo_vars.Items.Add("Variable_2");
             combo_vars.Items.Add("Variable_3");
+
+            //Einstellungen laden
+            ThemeManager.ChangeAppStyle(Application.Current,
+                            ThemeManager.GetAccent("Steel"),
+                            ThemeManager.GetAppTheme(Properties.Settings.Default.theme));
+
+            if (Properties.Settings.Default.theme == "BaseDark")
+            {
+                text_code_template.TextArea.Foreground = Brushes.White;
+                text_code_output.TextArea.Foreground = Brushes.White;
+                tg_theme.IsChecked = true;
+            }
+
+            text_code_template.ShowLineNumbers = Properties.Settings.Default.zeilennummern;
+            text_code_output.ShowLineNumbers = Properties.Settings.Default.zeilennummern;
+            tg_line_no.IsChecked = Properties.Settings.Default.zeilennummern;
+
+            text_code_template.Options.ShowSpaces = Properties.Settings.Default.leerzeichen;
+            tg_leerzeichen.IsChecked = Properties.Settings.Default.leerzeichen;
+
+            nc_font_size.Value = Properties.Settings.Default.schriftgrosse;
+            text_code_output.TextArea.FontSize = Properties.Settings.Default.schriftgrosse;
+            text_code_template.TextArea.FontSize = Properties.Settings.Default.schriftgrosse;
+
+
         }
 
         private void Btn_ersetzten_Click(object sender, RoutedEventArgs e)
@@ -209,9 +224,87 @@ namespace IECcodeGen
             text_code_template.Options.ShowSpaces = false;
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+
+
+        private void CopyToClipboard_Click_1(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(text_code_output.Text);
+        }
+
+
+        private void Tg_leerzeichen_IsCheckedChanged(object sender, EventArgs e)
+        {
+            if ((bool)tg_leerzeichen.IsChecked)
+            {
+                text_code_template.Options.ShowSpaces = true;
+
+                Properties.Settings.Default.leerzeichen = true;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                text_code_template.Options.ShowSpaces = false;
+
+                Properties.Settings.Default.leerzeichen = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Tg_theme_IsCheckedChanged(object sender, EventArgs e)
+        {
+            if ((bool)tg_theme.IsChecked)
+            {
+                ThemeManager.ChangeAppStyle(Application.Current,
+                                            ThemeManager.GetAccent("Steel"),
+                                            ThemeManager.GetAppTheme("BaseDark"));
+
+                text_code_template.TextArea.Foreground = Brushes.White;
+                text_code_output.TextArea.Foreground = Brushes.White;
+
+                Properties.Settings.Default.theme = "BaseDark";
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                ThemeManager.ChangeAppStyle(Application.Current,
+                                            ThemeManager.GetAccent("Steel"),
+                                            ThemeManager.GetAppTheme("BaseLight"));
+
+                text_code_template.TextArea.Foreground = Brushes.Black;
+                text_code_output.TextArea.Foreground = Brushes.Black;
+
+                Properties.Settings.Default.theme = "BaseLight";
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void NumericUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double?> e)
+        {
+            text_code_template.TextArea.FontSize = (double)nc_font_size.Value;
+            text_code_output.TextArea.FontSize = (double)nc_font_size.Value;
+
+            Properties.Settings.Default.schriftgrosse = (double)nc_font_size.Value;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Tg_line_no_IsCheckedChanged(object sender, EventArgs e)
+        {
+            if ((bool)tg_line_no.IsChecked)
+            {
+                text_code_template.ShowLineNumbers = true;
+                text_code_output.ShowLineNumbers = true;
+
+                Properties.Settings.Default.zeilennummern = true;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                text_code_template.ShowLineNumbers = false;
+                text_code_output.ShowLineNumbers = false;
+
+                Properties.Settings.Default.zeilennummern = false;
+                Properties.Settings.Default.Save();
+            }
         }
     }
 
