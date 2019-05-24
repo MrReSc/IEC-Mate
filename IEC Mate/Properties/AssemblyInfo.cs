1e7a0a807bf0d228858fa042c5fd64f0b1e12844
MrReSc <resc@mailbox.org>
2019-05-24 06:52:22 +0200

    Alle Libs auf 4.7.2 hochgezogen

--- IEC Mate/Properties/AssemblyInfo.cs
+++ IEC Mate/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.420.19144")]
-[assembly: AssemblyFileVersion("0.0.420.19144")]
+[assembly: AssemblyVersion("0.0.422.19144")]
+[assembly: AssemblyFileVersion("0.0.422.19144")]
