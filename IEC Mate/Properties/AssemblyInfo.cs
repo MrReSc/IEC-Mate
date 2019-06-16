df6d706535d3d29fbae4e33056c3d6b38a6959ec
MrReSc <resc@mailbox.org>
2019-06-16 07:47:48 +0200

    Release 0.1.1094

--- IEC Mate/Properties/AssemblyInfo.cs
+++ IEC Mate/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1094.19167")]
-[assembly: AssemblyFileVersion("0.0.1094.19167")]
+[assembly: AssemblyVersion("0.1.1095.19167")]
+[assembly: AssemblyFileVersion("0.1.1095.19167")]
