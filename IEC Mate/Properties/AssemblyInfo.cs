8fcca86323e03ed1803da1a18725e7a22a77a7ab
MrReSc <resc@mailbox.org>
2020-01-15 14:17:41 +0100

    column Tag nicht mehr grau #59

--- IEC Mate/Properties/AssemblyInfo.cs
+++ IEC Mate/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1521.20015")]
-[assembly: AssemblyFileVersion("1.0.1521.20015")]
+[assembly: AssemblyVersion("1.0.1522.20015")]
+[assembly: AssemblyFileVersion("1.0.1522.20015")]
