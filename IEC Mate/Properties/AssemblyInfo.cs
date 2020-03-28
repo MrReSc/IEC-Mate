814d35c45cf766d7f2271e31b2cc9100953e13f2
MrReSc <resc@mailbox.org>
2020-03-28 16:34:56 +0100

    bump to 1.0.1647

--- IEC Mate/Properties/AssemblyInfo.cs
+++ IEC Mate/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1645.20088")]
-[assembly: AssemblyFileVersion("1.0.1645.20088")]
+[assembly: AssemblyVersion("1.0.1647.20088")]
+[assembly: AssemblyFileVersion("1.0.1647.20088")]
